4a6dd3bf21eee16de7df30db7e53807a6179aad0
TirzGirlsBot <TirzGirlsBot@proton.me>
2025-05-28 21:27:51 -0400

    z

---
+++ commands/shade.py
@@ -0,0 +1,2 @@
+async def handle(update, context):
+    await update.message.reply_text("I would agree with you but then we’d both be wrong.")
---
+++ commands/vibecheck.py
@@ -0,0 +1,2 @@
+async def handle(update, context):
+    await update.message.reply_text("The vibe today? A lil tired, a lil chaotic, and somebody definitely needs a snack. Y’all good?")
