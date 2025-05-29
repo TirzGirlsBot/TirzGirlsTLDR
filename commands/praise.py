70f3213789e1f60aef74636a712ee6a9969f7d87
TirzGirlsBot <TirzGirlsBot@proton.me>
2025-05-28 21:27:04 -0400

    x

--- commands/praise.py
+++
@@ -1,2 +0,0 @@
-async def handle(update, context):
-    await update.message.reply_text("You look like a limited edition right now. Keep glowing.")
--- commands/shade.py
+++
@@ -1,2 +0,0 @@
-async def handle(update, context):
-    await update.message.reply_text("I would agree with you but then we’d both be wrong.")
