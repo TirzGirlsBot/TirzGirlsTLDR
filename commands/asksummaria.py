60c3a6fcb655d8424c80ff44b5d4499ea6bf039d
TirzGirlsBot <TirzGirlsBot@proton.me>
2025-05-28 21:33:48 -0400

    s

--- commands/asksummaria.py
+++
@@ -1,6 +0,0 @@
-async def handle(update, context):
-    query = ' '.join(context.args)
-    if not query:
-        await update.message.reply_text("Ask me something juicy, babe.")
-    else:
-        await update.message.reply_text(f"Sis... I heard: {query}. Let me see... That’s a hard pass.")
--- commands/summariadvice.py
+++
@@ -1,2 +0,0 @@
-async def handle(update, context):
-    await update.message.reply_text("If the shoe fits, great. If it gives you blisters, throw the whole situationship out.")
